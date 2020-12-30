feb3c622796338f41e7432fc2bc478981ad17880
Bill Liu <liu_bill@live.com>
2020-12-29 17:54:37 -0800

    Sort blog posts:
    1. Add chrono crate to sort by NaiveDate
    2. Add unit tests

--- src/blog.rs
+++ src/blog.rs
@@ -1,6 +1,8 @@
 use crate::error;
+use chrono::naive::NaiveDate;
 use comrak;
-use serde::{Deserialize};
+use serde::Deserialize;
+use std::str::FromStr;
 use toml;
 use toml::value::Datetime;
 
@@ -8,7 +10,7 @@ pub struct Blog {
     pub title: String,
     pub descr: String,
     pub url_friendly_name: String,
-    pub date: Datetime,
+    pub date: NaiveDate,
     pub content: String,
 }
 
@@ -18,12 +20,10 @@ pub struct BlogReader {
 
 impl BlogReader {
     pub fn new() -> BlogReader {
-        BlogReader {
-            blogs: vec!()
-        }
+        BlogReader { blogs: vec![] }
     }
 
-    pub fn ingest(& mut self, blog: String) -> error::EmptyResult {
+    pub fn ingest(&mut self, blog: String) -> error::EmptyResult {
         let blog_divided: Vec<_> = blog.splitn(3, "---").collect();
         if blog_divided.len() < 3 {
             return Err(Box::new(error::SiteError {
@@ -34,16 +34,16 @@ impl BlogReader {
             }));
         }
         let metadata: BlogMetadata = toml::from_str(blog_divided[1])?;
+
         self.blogs.push(Blog {
-            content: comrak::markdown_to_html(
-                blog_divided[2],
-                &comrak::ComrakOptions::default(),
-            ),
+            content: comrak::markdown_to_html(blog_divided[2], &comrak::ComrakOptions::default()),
             title: metadata.title,
             descr: metadata.descr,
             url_friendly_name: metadata.url_friendly_name,
-            date: metadata.date,
+            date: NaiveDate::from_str(&metadata.date.to_string())?,
         });
+
+        self.blogs.sort_by(|a, b| b.date.cmp(&a.date));
         return Ok(());
     }
 
@@ -58,4 +58,86 @@ struct BlogMetadata {
     descr: String,
     url_friendly_name: String,
     date: Datetime,
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_ingest() -> error::EmptyResult {
+        let mut subject = BlogReader::new();
+
+        subject.ingest(String::from(
+"---
+title = \"Test Title\"
+descr = \"Test description.\"
+url_friendly_name = \"url-friendly\"
+date = 2020-01-01
+---
+This is a test
+
+With Markdown
+"))?;
+
+        assert!(subject.get_blogs().len() == 1);
+        assert!(subject.get_blogs()[0].title == "Test Title");
+        assert!(subject.get_blogs()[0].descr == "Test description.");
+        assert!(subject.get_blogs()[0].url_friendly_name == "url-friendly");
+        // This assertion is highly dependent on the Markdown library.
+        // It's easier to get the output first, then set it as the correct value.
+        assert!(subject.get_blogs()[0].content ==
+"<p>This is a test</p>
+<p>With Markdown</p>
+");
+        
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_blogs_correct_order() -> error::EmptyResult {
+        let mut subject = BlogReader::new();
+
+        subject.ingest(String::from(
+"---
+title = \"Test 1\"
+descr = \"Test.\"
+url_friendly_name = \"url-friendly\"
+date = 2020-01-01
+---
+This is a test
+
+With Markdown
+"))?;
+        subject.ingest(String::from(
+"---
+title = \"Test 2\"
+descr = \"Test.\"
+url_friendly_name = \"url-friendly\"
+date = 2020-01-03
+---
+This is a test
+
+With Markdown
+"))?;
+        subject.ingest(String::from(
+"---
+title = \"Test 3\"
+descr = \"Test.\"
+url_friendly_name = \"url-friendly\"
+date = 2020-01-02
+---
+This is a test
+
+With Markdown
+"))?;
+        
+        assert!(subject.get_blogs().len() == 3);
+        assert!(subject.get_blogs()[0].title == "Test 2");
+        assert!(subject.get_blogs()[1].title == "Test 3");
+        assert!(subject.get_blogs()[2].title == "Test 1");
+
+        Ok(())
+    }
 }
